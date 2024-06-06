e05d206b3245aea6790c8a5bc5ee74c575232205
Danny Vivas <dvivasmera1.2@gmail.com>
2024-06-06 10:16:55 -0500

    adjusts

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,9 +11,9 @@ const Homepage = () => {
       animate={{ y: '0%' }}
       transition={{ duration: 1 }}
     >
-      <div className="  h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* IMAGE CONTAINER */}
-        <div className="md:w-1/2  lg:h-full lg:w-1/2 relative">
+        <div className="h-1/2 lg:h-full lg:w-1/2 relative">
           <Image src="/hero.png" alt="" fill className="object-contain" />
         </div>
         {/* TEXT CONTAINER */}

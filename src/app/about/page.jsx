ce4d30479b75aaa578d45aa94a71315a0d6c548e
Danny Vivas <dvivasmera1.2@gmail.com>
2024-06-06 10:06:22 -0500

    adjusts

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -23,7 +23,7 @@ export default function Aboutpage() {
       transition={{ duration: 1 }}
     >
       {/* CONTAINER */}
-      <div className="h-full overflow-scroll lg:flex" ref={containerRef}>
+      <div className="min-h-screen h-full overflow-scroll lg:flex" ref={containerRef}>
         {/* TEXT CONTAINER */}
         <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:px-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:1/2">
           {/* BIOGRAPHY CONTAINER */}
